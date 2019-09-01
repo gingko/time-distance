2453840214ca04d895abd538dd7ac3f562939aca
Adriano Ferrari <adriano.ferrari@gmail.com>
2019-09-01 08:21:30 -0400

    Documentation added for Time.Distance.Types

--- src/Time/Distance/Types.elm
+++ src/Time/Distance/Types.elm
@@ -1,6 +1,11 @@
 module Time.Distance.Types exposing (Config, DistanceId(..), Locale, Tense(..))
 
+{-|
+    @docs DistanceId, Tense, Config, Locale
+-}
 
+{-|All available time intervals.
+-}
 type DistanceId
     = LessThanXSeconds Int
     | HalfAMinute
@@ -14,15 +19,20 @@ type DistanceId
     | OverXYears Int
     | AlmostXYears Int
 
-
+{-|Is this a time in the past, or the future?
+-}
 type Tense
     = Past
     | Future
 
 
+{-|Should we include affix ("... ago", "in ...")?
+-}
 type alias Config =
     { withAffix : Bool }
 
 
+{-|Main function type : take all the above, return a string.
+-}
 type alias Locale =
     Config -> Tense -> DistanceId -> String
